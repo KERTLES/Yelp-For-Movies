df215c4a6e2d34225e5e88fc4bf2ad8a8da0b0b5
Matthew Oshimo <moshimo90696@gmail.com>
2022-08-19 15:34:17 -0700

    fixed docker issue

--- accounts/api/account_project/settings.py
+++ accounts/api/account_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'account_rest.apps.AccountRestConfig',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
