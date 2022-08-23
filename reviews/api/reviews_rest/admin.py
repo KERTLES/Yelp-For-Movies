41c285798528176a12f2a7d3e2f99e42cdb4af05
Kelly <kellyjm2002@hotmail.com>
2022-08-23 14:04:02 -0700

    Created views for list reviews and get reviews by movie id. filter by imdb_id not working, but is filtering by review id and movie id. - with Nikansha

--- reviews/api/reviews_rest/admin.py
+++ reviews/api/reviews_rest/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Review, Movie
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+
--- reviews/api/reviews_rest/views.py
+++ reviews/api/reviews_rest/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+
+from common.json import ModelEncoder
+
+from .models import Review, Movie
 # Create your views here.
+
+
+class MovieEncoder(ModelEncoder):
+    model = Movie
+    properties = [
+        "imdb_id",
+        # "title",
+        # "year",
+        # "rated",
+        # "released",
+        # "runtime",
+        # "genre",
+        # "director",
+        # "writer",
+        # "actors",
+        # "plot"
+    ]
+
+
+class ReviewsEncoder(ModelEncoder):
+    model = Review
+    properties = ["title", "post", "imdb_id"]
+
+    encoders =  {"imdb_id": MovieEncoder()}
+    
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_reviews(request, movie_id):
+    if request.method == "GET":
+
+        if movie_id != None:
+            try:
+                
+                reviews = Review.objects.get(imdb_id=movie_id)
+                # movie = Movie.objects.get(imdb_id=reviews.imdb_id)
+
+            except Movie.DoesNotExist:
+                return JsonResponse(
+                    {"message": "invalid movie id"}
+                )
+            return JsonResponse(
+                reviews,
+                encoder=ReviewsEncoder,
+                safe=False
+            )
+        else:
+
+            reviews = Review.objects.all()
+        
+
+            return JsonResponse({"reviews:": reviews}, encoder=ReviewsEncoder)
+
+    # else:
+    #     content = json.loads(request.body)
+    #     try:
+    #         movie = Movie.objects.get(imdb_id=movie_id)
+    #         content["imdb_id"] = movie
+    #     except Movie.DoesNotExist:
+    #         return JsonResponse(
+    #             {
+    #                 "message": "invalid movie id",
+
+    #             },
+    #             status=400
+    #         )
+
+    #     review = Review.objects.create(**content)
+    #     return JsonResponse(
+    #         review, encoder=ReviewsEncoder, safe=False
+    #     )
