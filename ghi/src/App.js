7c3b443dd6c672bceb4f2e1bba3887243c4aab6a
myolin <myolin1437@gmail.com>
2022-08-22 22:27:53 -0700

    Able to display a list of trending movie for homepage

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DropdownNav from './DropdownNav';
+import HomePage from './HomePage';
 
-function App() {
+
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <DropdownNav />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="manufacturers">
+            <Route path="" element={<ManufacturersList />} />
+            <Route path="new" element={<ManufacturerForm />} />
+          </Route> */}
+        </Routes> 
+      </div>
+    </BrowserRouter>
   );
 }
 
