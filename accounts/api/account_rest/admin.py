7d147df1786ef7ebb5b10b9a886a29103f7e0a4d
Lander Nunez <landernunez@gmail.com>
2022-08-23 16:56:58 -0500

    changes to admin.py

--- accounts/api/account_rest/admin.py
+++ accounts/api/account_rest/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import AccountVO
 
-@admin.register(Account)
+@admin.register(AccountVO)
 class AccountAdmin(admin.ModelAdmin):
     pass
 
