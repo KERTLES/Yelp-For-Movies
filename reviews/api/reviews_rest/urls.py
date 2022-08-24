2afbfdd424424ffcb855db9cb0a2e9a158b80861
Kelly <kellyjm2002@hotmail.com>
2022-08-24 15:31:22 -0700

    Fixed urls for PUT and DELETE with Nikansha. Added list movies view and urls.

--- reviews/api/reviews_rest/urls.py
+++ reviews/api/reviews_rest/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import api_list_reviews
+from .views import api_list_reviews, api_show_review, api_list_movies
 
 urlpatterns = [
+    path("movies/", api_list_movies, name="list_movies"),
     path("reviews/", api_list_reviews, name="list_reviews"),
-    path("reviews/<int:movieId>/", api_list_reviews, name="review_by_id"),
-    path("create/<int:movieId>/", api_list_reviews, name="create"),
+    path("reviews/<int:movie_id>/", api_list_reviews, name="review_by_id"),
+    path("create/<int:movie_id>/", api_list_reviews, name="create"),
 
+    path("reviews/edit/<int:pk>/", api_show_review, name="edit_review"), #PUT
+    path("reviews/delete/<int:pk>/", api_show_review, name="delete_review"), #DELETE
 ]
--- reviews/api/reviews_rest/views.py
+++ reviews/api/reviews_rest/views.py
@@ -16,7 +16,7 @@ class MovieEncoder(ModelEncoder):
     model = Movie
     properties = [
         "imdb_id",
-        # "title",
+        "title",
         # "year",
         # "rated",
         # "released",
@@ -36,14 +36,25 @@ class ReviewsEncoder(ModelEncoder):
     encoders = {"movie": MovieEncoder()}
 
 
+
+@require_http_methods(["GET"])
+def api_list_movies(request):
+    if request.method == "GET":
+        
+        movies = Movie.objects.all()
+        return JsonResponse({"movies:": movies}, encoder=MovieEncoder)
+
+  
+
+
 @require_http_methods(["GET", "POST"])
-def api_list_reviews(request, movieId=None):
+def api_list_reviews(request, movie_id=None):
     if request.method == "GET":
 
-        if movieId != None:
+        if movie_id != None:
             try:
 
-                reviews = Review.objects.filter(movie=movieId)
+                reviews = Review.objects.filter(movie=movie_id)
 
             except Movie.DoesNotExist:
                 return JsonResponse(
@@ -62,7 +73,7 @@ def api_list_reviews(request, movieId=None):
     else:
         content = json.loads(request.body)
         try:
-            movie = Movie.objects.get(id=movieId)
+            movie = Movie.objects.get(id=movie_id)
 
         except Movie.DoesNotExist:
             return JsonResponse(
@@ -76,4 +87,33 @@ def api_list_reviews(request, movieId=None):
         review = Review.objects.create(**content)
         return JsonResponse(
             review, encoder=ReviewsEncoder, safe=False
+        )
+
+
+@require_http_methods(["DELETE", "PUT"])
+def api_show_review(request, pk):
+    
+    if request.method == "DELETE":
+        count, _ = Review.objects.filter(id=pk).delete()
+        return JsonResponse ({"deleted": count > 0})
+    
+    else: #PUT
+        content = json.loads(request.body)
+
+        try:
+            if "movie" in content:
+                movie = Movie.objects.get(id=content["movie"])
+                content["movie"] = movie
+        except Movie.DoesNotExist:
+            return JsonResponse(
+                {"message": "invalid movie"},
+                status=400
+            )
+        
+        Review.objects.filter(id=pk).update(**content)
+        review = Review.objects.get(id=pk)
+        return JsonResponse(
+            review,
+            encoder=ReviewsEncoder,
+            safe=False,
         )
