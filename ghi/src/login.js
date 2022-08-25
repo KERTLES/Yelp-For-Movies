efb04ac0b254196edcb3736bdd4a631d5d36ba85
Lander Nunez <landernunez@gmail.com>
2022-08-25 11:23:56 -0500

    Login stuff

---
+++ ghi/src/login.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('login-form');
+    form.addEventListener('submit', async event => {
+      event.preventDefault();
+  
+      const fetchOptions = {
+        method: 'post',
+        body: new FormData(form),
+        credentials: 'include',
+      };
+
+      const url = 'http://localhost:8080/accounts/';
+      const response = await fetch(url, fetchOptions);
+      if (response.ok) {
+        window.location.href = '/';
+      } else {
+        console.error(response);
+      }
+    });
+  });
+  
