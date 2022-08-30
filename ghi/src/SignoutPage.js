cb1d812e2504f995e7f97e17c85b6cf78efe94e6
Lander Nunez <landernunez@gmail.com>
2022-08-30 14:30:04 -0500

    Signout.js

---
+++ ghi/src/SignoutPage.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext, useToken } from "./token";
+import { useNavigate } from "react-router-dom";
+
+function logout() {
+    return logout()
+}
+return logout()
