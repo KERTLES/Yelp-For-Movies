cfd7f0a85299b8075fabeb058f1209ed3cf3818c
Lander Nunez <landernunez@gmail.com>
2022-09-02 14:46:43 -0500

    More hook changes

--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { AiOutlineInstagram } from "react-icons/ai";
 
 function Footer(props) {
     
@@ -35,14 +36,13 @@ function Footer(props) {
       <div className="row mt-3">
         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <h6 className="text-uppercase fw-bold mb-4">
-            <i className="fas fa-gem me-3"></i>Company name
+            <i className="fas fa-gem me-3"></i>About us
           </h6>
           <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
+          Yovies is dedicated to providing movie-goers the best place to share their opinions on up and coming movies.
           </p>
         </div>
-        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        {/* <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <h6 className="text-uppercase fw-bold mb-4">
             Products
           </h6>
@@ -75,23 +75,23 @@ function Footer(props) {
           <p>
             <a href="#!" className="text-reset">Help</a>
           </p>
-        </div>
+        </div> */}
         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <h6 className="text-uppercase fw-bold mb-4">Contact Us</h6>
+          <p><i className="fas fa-home me-3"></i> Los Angeles, CA  90013, US</p>
           <p>
             <i className="fas fa-envelope me-3"></i>
-            info@example.com
+            Yovies@gmail.com
           </p>
-          <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+          {/* <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p> */}
+          {/* <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p> */}
         </div>
       </div>
     </div>
   </section>
   <div className="text-center p-4" style={{backgroundColor:"rgba(0, 0, 0, 0.05)"}}>
-    © 2021 Copyright:
-    <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+
+<a className="text-reset fw-bold" href="yovies.com">Yovies.com</a>
   </div>
 </footer>
 </>
