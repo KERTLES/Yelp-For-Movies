1f8b7685401493d911c6578dba201487ca0bb8e5
myolin <myolin1437@gmail.com>
2022-09-06 15:44:13 -0700

    Changed name

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,7 +5,7 @@ import CreateReviewForm from './CreateReviewForm';
 import MovieDetail from './MovieDetail';
 import GenreList from './GenreList';
 import SignupPage from './SignupPage';
-import Login from './Login';
+import Login from './login';
 import { AuthProvider } from "./token";
 import UserProfile from './UserProfile';
 import Footer from './Footer';
