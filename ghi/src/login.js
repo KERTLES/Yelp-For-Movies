4948de3940595c64f5044702f5d8f8e3830b51de
myolin <myolin1437@gmail.com>
2022-09-06 14:42:16 -0700

    WIP

--- ghi/src/login.js
+++ ghi/src/login.js
@@ -104,7 +104,7 @@ function handleSubmit(event)
 
   return(
   <section className="vh-100" >
-  <div title="login" className="container h-100">
+  <div title="login" className="container h-200">
     <div className="row d-flex justify-content-center align-items-center h-100">
       <div className="col-lg-12 col-xl-11">
         <div className="card text-black" >
@@ -150,8 +150,8 @@ function handleSubmit(event)
               </div>
               <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                  className="img-fluid" alt="Sample image" />
+                <img src="/yoovieswhite.png"
+                  className="img-fluid" alt="logo" />
 
               </div>
             </div>
