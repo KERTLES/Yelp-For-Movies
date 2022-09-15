1af943da530da90a3c55a8f900edbe89263504d1
myolin <myolin1437@gmail.com>
2022-09-15 10:34:05 -0700

    Made small css styling adjustments

--- ghi/src/HomePage.js
+++ ghi/src/HomePage.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react"
 import { HiOutlineSearch } from "react-icons/hi";
-import { FaArrowRight } from "react-icons/fa";
+import { IoArrowForwardOutline } from "react-icons/io5";
 import MoviesList from "./MoviesList";
 
 
 function HomePage() {
   const [movieName, setMovieName] = useState("")
-
+addd
   const handleSubmit = (event) => {
     event.preventDefault()
     setMovieName(event.target.movie.value.toLowerCase())
@@ -19,9 +19,19 @@ function HomePage() {
           <img src={`${process.env.PUBLIC_URL}/yoovieswhite.png`} alt="logo" width="500" height="auto" />
         </a>
         <form onSubmit={handleSubmit} className="d-flex mt-3 mb-3">
-            <HiOutlineSearch style={{fontSize: "45px", color: "white"}} />
-            <input placeholder="Enter a movie..." type="search" name="movie" id="movie" aria-label="Search" className="form-control form-control-lg me-2"/>
-            <button className="btn btn-light me-2 rounded-circle" type="submit"><FaArrowRight style={{fontSize: "22px", color: "black"}} /></button>
+            <HiOutlineSearch style={{fontSize: "50px", color: "white"}} />
+            <input 
+              placeholder="Enter a movie..." 
+              type="search" 
+              name="movie" 
+              id="movie" 
+              aria-label="Search" 
+              className="form-control form-control-lg mx-2"
+            />
+            <button 
+              className="btn btn-light rounded-circle p-2" type="submit" style={{outline: "none", border: "none"}}>
+              <IoArrowForwardOutline style={{fontSize: "35px"}} />
+            </button>
         </form>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4 text-white">
