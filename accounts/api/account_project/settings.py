6a7b6fbc2fb3cb743e396c29a3ae9c990f927006
Matthew Oshimo <moshimo90696@gmail.com>
2022-10-18 19:27:56 -0700

    remove keys

--- accounts/api/account_project/settings.py
+++ accounts/api/account_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!3vzjusw@pf)h9xssbaete35vmb6fp4&b@fj%cz!(h4*b70ar'
+SECRET_KEY = os.environ.get('SECRET_KEY_ACCOUNTS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False) == "True"
--- reviews/api/reviews_project/settings.py
+++ reviews/api/reviews_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0p=3mve68&slozzmupptx(b=+b=9_53p(b64d*x)nrlo67ew2w"
+SECRET_KEY = os.environ.get('SECRET_KEY_REVIEWS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False) == "True"
